d072e2b7ad476fa8cd1413de6a076f0dae295d3d
Tim van der Lippe <tvanderlippe@gmail.com>
2015-09-12 13:52:03 +0200

    Add Javadoc to API methods and interfaces.

--- src/main/java/throwinglambdas/functional/ThrowingConsumer.java
+++ src/main/java/throwinglambdas/functional/ThrowingConsumer.java
@@ -1,6 +1,19 @@
 package throwinglambdas.functional;
 
+/**
+ * A function that accepts input of type {@code T} and might throw exceptions of type {@code E}.
+ *
+ * @param <T> The type of input.
+ * @param <E> The type of exceptions thrown.
+ */
 @FunctionalInterface
 public interface ThrowingConsumer<T, E extends Throwable> {
+
+    /**
+     * Execute the method.
+     *
+     * @param t The input to the method.
+     * @throws E The exception that might be thrown.
+     */
     void accept(T t) throws E;
 }
--- src/main/java/throwinglambdas/functional/ThrowingFunction.java
+++ src/main/java/throwinglambdas/functional/ThrowingFunction.java
@@ -1,6 +1,22 @@
 package throwinglambdas.functional;
 
+/**
+ * A method that accepts input of type {@code T}, returns output of type {@code R}
+ * and might throw exceptions of type {@code E}.
+ *
+ * @param <T> The type of input.
+ * @param <R> The type of output.
+ * @param <E> The type of exceptions thrown.
+ */
 @FunctionalInterface
 public interface ThrowingFunction<T, R, E extends Throwable> {
+
+    /**
+     * Execute the method.
+     *
+     * @param t The input to the method.
+     * @return The output of the method.
+     * @throws E The exception that might be thrown.
+     */
     R apply(T t) throws E;
 }
